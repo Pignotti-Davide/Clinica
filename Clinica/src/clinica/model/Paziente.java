ddc57dd4dcca26c9f4daa4a8ef086009dccea39f
Davide Pignotti <davide110494@gmail.com>
2016-05-13 12:54:21 +0200

    model

---
+++ Clinica/src/clinica/model/Paziente.java
@@ -0,0 +1,15 @@
+package clinica.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Paziente {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long idTipologiaEsame;
+	@Column(nullable=false)
+	private String nome;
+	@Column(nullable=false)
+	private String cognome;
+}
---
+++ Clinica/src/clinica/model/TipologiaEsame.java
@@ -0,0 +1,23 @@
+package clinica.model;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.*;
+
+@Entity
+public class TipologiaEsame {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long idTipologiaEsame;
+	@Column(nullable=false)
+	private String nome;
+	@Column(nullable=false)
+	private String descrizione;
+	@Column(nullable=false)
+	private double costo;
+	@OneToMany
+	private Map<String,String> prerequisiti;
+	@OneToMany
+	private List<String> indicatoriRisultati;
+}
