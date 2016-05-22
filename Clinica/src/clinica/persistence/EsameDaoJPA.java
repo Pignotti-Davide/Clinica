38a4d22ab8ffa97c9f25ee7f0daf87a891c098ab
Susysk <sus.marinai@hotmail.it>
2016-05-22 13:04:14 +0200

    Update EsameDaoJPA.java

--- Clinica/src/clinica/persistence/EsameDaoJPA.java
+++ Clinica/src/clinica/persistence/EsameDaoJPA.java
@@ -1,10 +1,40 @@
 package clinica.persistence;
 
 public class EsameDaoJPA {
+	public void create(Esame e) {
 
-	
-	public void create(){
-		
+		try{
+			Connessione.getEm().getTransaction().begin();
+			Connessione.getEm().persist(e);
+			Connessione.getEm().getTransaction().commit();
+			Connessione.getEm().clear();				
+		}
+		catch (RollbackException e){
+			Connessione.getEm().clear();
+		}	
 	}
-	
+
+	public Esame retrieve(int id) {
+		Esame es=null;
+		Connessione.getEm().getTransaction().begin();
+		es = Connessione.getEm().find(Esame.class, id);
+		Connessione.getEm().clear();
+		Connessione.getEm().getTransaction().commit();
+		return es;
+	}
+
+	public void update(Esame e) {
+		Connessione.getEm().getTransaction().begin();
+		Connessione.getEm().merge(e);
+		Connessione.getEm().getTransaction().commit();
+	}
+
+	public void delete(Esame e) {
+		Esame esa=null;
+		Connessione.getEm().getTransaction().begin();
+		esa=Connessione.getEm().find(Medico.class, e.getIdMedico());
+		Connessione.getEm().remove(esa);
+		Connessione.getEm().getTransaction().commit();
+	}
 }
+
