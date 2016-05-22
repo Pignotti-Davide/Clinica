2d7d6b3644e52e6007b41d186ae6518b581ceb89
Susysk <sus.marinai@hotmail.it>
2016-05-22 14:47:49 +0200

    Update TipologiaEsameDaoJPA.java

--- Clinica/src/clinica/persistence/TipologiaEsameDaoJPA.java
+++ Clinica/src/clinica/persistence/TipologiaEsameDaoJPA.java
@@ -1,4 +1,41 @@
+package clinica.persistence;
+import javax.persistence.RollbackException;
 
+import clinica.model.TipologiaEsame;
 public class TipologiaEsameDaoJPA {
+	public void create(TipologiaEsame e) {
 
+		try{
+			Connessione.getEm().getTransaction().begin();
+			Connessione.getEm().persist(e);
+			Connessione.getEm().getTransaction().commit();
+			Connessione.getEm().clear();				
+		}
+		catch (RollbackException e){
+			Connessione.getEm().clear();
+		}	
+	}
+
+	public TipologiaEsame retrieve(int id) {
+		TipologiaEsame es=null;
+		Connessione.getEm().getTransaction().begin();
+		es = Connessione.getEm().find(TipologiaEsameclass, id);
+		Connessione.getEm().clear();
+		Connessione.getEm().getTransaction().commit();
+		return es;
+	}
+
+	public void update(TipologiaEsamee) {
+		Connessione.getEm().getTransaction().begin();
+		Connessione.getEm().merge(e);
+		Connessione.getEm().getTransaction().commit();
+	}
+
+	public void delete(TipologiaEsamee) {
+		TipologiaEsame esa=null;
+		Connessione.getEm().getTransaction().begin();
+		esa=Connessione.getEm().find(TipologiaEsame.class, e.getId());
+		Connessione.getEm().remove(esa);
+		Connessione.getEm().getTransaction().commit();
+	}
 }
